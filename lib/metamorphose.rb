7a0c65f1b003f77935c6d710e8153a07475e9593
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-09-16 23:17:09 +0900

    add classes representing objects returned by Ripper.sexp

--- lib/metamorphose.rb
+++ lib/metamorphose.rb
@@ -1,5 +1,48 @@
 require "metamorphose/version"
 
 module Metamorphose
-  # Your code goes here...
+
+  class RipperSexp
+    AT_MARK = "@"
+
+    def initialize sexp
+      @sexp = sexp
+    end
+
+    def event_name
+      @sexp.first
+    end
+
+    class << self
+      def self.detect sexp
+        first = sexp.first
+        case first
+        when Array
+          self.detect first
+        when Symbol
+          if first[0] == AT_MARK
+            ScannerEvent.new( sexp )
+          else
+            ParserEvent.new( sexp )
+          end
+        else
+          raise ArgumentError, "#{sexp.inspect} doesn't seem to be a S expression!"
+        end
+      end
+    end
+
+    class ScannerEvent < RipperSexp
+      def token
+        @sexp[1]
+      end
+      def location_info
+        @sexp[2]
+      end
+    end
+
+    class ParserEvent < RipperSexp
+    end
+
+  end
+
 end
