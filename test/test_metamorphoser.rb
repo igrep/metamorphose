8ebf376e57dfb2baf813d38cb7a526a896464fae
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-10-14 00:30:45 +0900

    add the metamorphosed source to test_metamorphoser.rb

--- test/test_metamorphoser.rb
+++ test/test_metamorphoser.rb
@@ -4,4 +4,9 @@ require 'metamorphose'
 require 'awesome_print'
 
 m = Metamorphose::Metamorphoser.new File.open( ARGV[0] )
+
+puts "parsing result: "
 ap m.parse
+
+puts "metamorphosed_source: "
+puts m.metamorphosed_source
